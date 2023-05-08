214ce47ebfcad163c5af05ed9b22c7a2b743881d
Marco Mattei <marco.mattei@migrosonline.ch>
2023-05-08 11:50:58 +0200

    Part 2 of the workshop

--- iOSWorkshop/Data/Model/PeopleDTO.swift
+++ iOSWorkshop/Data/Model/PeopleDTO.swift
@@ -1,11 +1,40 @@
 struct PeopleDTO: Codable {
-    //TODO: Part 1 - Write the PeopleDTO struct according to the data model. Required fields are defined in the "People" domain object
+    // Part 1 - Write the PeopleDTO struct according to the data model. Required fields are defined in the "People" domain object
+    let name: String
+    let height: String
+    let mass: String
+    let hairColor: String
+    let skinColor: String
+    let eyeColor: String
+    let birthYear: String
+    let gender: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "name"
+        case height = "height"
+        case mass = "mass"
+        case hairColor = "hair_color"
+        case skinColor = "skin_color"
+        case eyeColor = "eye_color"
+        case birthYear = "birth_year"
+        case gender = "gender"
+    }
 }
 
 extension PeopleDTO {
     func toDomain() -> People? {
-        // TODO: Part 1 - Convert the DTO to the domain model
-        // ⚠️ If the conversion of the "height" or "width" attributes to Int fail, return nil
-        return nil
+        // Part 1 - Convert the DTO to the domain model
+        let height = Int(height)
+        let mass = Int(mass)
+        guard let height, let mass else { return nil }
+        
+        return People(name: name,
+                      height: height,
+                      mass: mass,
+                      hairColor: hairColor,
+                      skinColor: skinColor,
+                      eyeColor: eyeColor,
+                      birthYear: birthYear,
+                      gender: gender)
     }
 }
