01dd79ecadbb278917c744c9c88168bba2680eff
Marco Mattei <marco.mattei@migrosonline.ch>
2023-05-08 12:11:06 +0200

    Part 9 of the workshop

--- iOSWorkshop/Data/DataSource/NetworkPeopleDataSource.swift
+++ iOSWorkshop/Data/DataSource/NetworkPeopleDataSource.swift
@@ -6,23 +6,21 @@ class NetworkPeopleDataSource: PeopleDataSource {
         let url = URL(string: "https://swapi.dev/api/people")!
         let urlRequest = URLRequest(url: url)
         
-        //TODO: Part 8
+        // Part 8
         /*
          Use URLSession to perform a data request. Then use JSONDecoder() to decode the data into a "PeopleListResultDTO".
          Throw the correct domain error according the the actual error
          */
-        /*
         do {
-            let (data, _) = //TODO: Perform data request
-            let peopleListResult = //TODO: Decode the data
+            let (data, _) = try await URLSession.shared.data(for: urlRequest)
+            let peopleListResult = try JSONDecoder().decode(PeopleListResultDTO.self, from: data)
             return peopleListResult.results
         } catch let error as URLError where error.code == URLError.Code.notConnectedToInternet {
-            //TODO: Throw an error
+            throw PeopleError.noInternet
         } catch {
-            //TODO: Throw an error
+            throw PeopleError.unknown
         }
-         */
-        return []
+
     }
         
 }
