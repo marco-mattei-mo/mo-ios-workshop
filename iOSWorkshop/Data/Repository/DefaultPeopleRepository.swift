ce0ad4d99936149415da2b2b44e0b9cc35bae326
Marco Mattei <marco.mattei@migrosonline.ch>
2023-05-08 11:53:36 +0200

    Part 4 of the workshop

--- iOSWorkshop/Data/Repository/DefaultPeopleRepository.swift
+++ iOSWorkshop/Data/Repository/DefaultPeopleRepository.swift
@@ -2,7 +2,8 @@ struct DefaultPeopleRepository: PeopleRepository {
     let peopleDataSource: PeopleDataSource
     
     func findPeopleList() async throws -> [People] {
-        //TODO: Part 3 - Call the datasource function to find the people list and convert the data to match the return type
+        // Part 3 - Call the datasource function to find the people list and convert the data to match the return type
+        try await peopleDataSource.findPeopleList().compactMap { $0.toDomain() }
     }
        
 }
